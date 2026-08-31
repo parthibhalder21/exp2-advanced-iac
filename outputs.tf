4e595b903e7459b4ae841ab31f3962bd38c259b9
PARTH <145361417+parthibhalder21@users.noreply.github.com>
2026-08-31 16:43:48 +0530

    Add outputs for web IPs, DB IP, and VPC ID

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "web_public_ips" {
+  value = module.compute.web_public_ips
+}
+
+output "db_private_ip" {
+  value = module.compute.db_private_ip
+}
+
+output "vpc_id" {
+  value = module.network.vpc_id
+}
